d5bb9d4eeb656c7c8eb0bd9854e0ff6ab12efe31
caoqiang <caoqiang@aikucun.com>
2020-10-20 14:50:50 +0800

    first commit

---
+++ src/main/java/com/troy/algorithm/Test.java
@@ -0,0 +1,13 @@
+package com.troy.algorithm;
+
+/**
+ * @author caoqiang
+ * @date 2020-10-20 14:49
+ * @desc
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("fuck algorithm");
+    }
+}
