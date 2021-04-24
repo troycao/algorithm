ef7521773863e3416182b01460866818ede9949b
troy <caoq1987@gmail.com>
2021-04-24 10:11:41 +0800

    rbtree

---
+++ src/main/java/com/troy/structure/tree/balance/BalanceBinarySearchTree.java
@@ -0,0 +1,7 @@
+package com.troy.structure.tree.balance;/**
+ * @Author troy
+ * @Date 2021/1/31 1:41 下午
+ * @Version 1.0
+ * @Desc:
+ */public class BalanceBinarySearchTree {
+}
---
+++ src/main/java/com/troy/structure/tree/balance/redBlackTree/RedBlackTree.java
@@ -0,0 +1,34 @@
+package com.troy.structure.tree.redBlackTree;
+
+import com.troy.structure.tree.binarySearchTree.BinarySearchTree;
+
+import java.util.Comparator;
+
+/**
+ * @Author troy
+ * @Date 2021/1/31 12:52 下午
+ * @Version 1.0
+ * @Desc: 红黑树
+ */
+public class RedBlackTree<E> extends BinarySearchTree<E> {
+
+    private Comparator<E> comparator;
+
+    public RedBlackTree(){
+        this(null);
+    }
+
+    public RedBlackTree(Comparator<E> comparator){
+        super();
+        this.comparator = comparator;
+    }
+
+    private static class RedBlackNode<E> extends Node<E>{
+
+        public RedBlackNode(E element, Node<E> parent) {
+            super(element, parent);
+        }
+    }
+
+
+}
